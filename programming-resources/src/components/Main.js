403ac795368d88944a77ca0e9e0b934b8f3e5e3d
Adam Hwung <Ahwung@gmail.com>
2019-12-13 22:55:57 -0800

    Connected all files together and rendered test view

---
+++ programming-resources/src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Submission from './Submission.js'
+import Form from './Form.js'
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+          <Submission />
+          <Form />
+      </div>
+    )
+  }
+}
+
+
+export default Main;
---
+++ programming-resources/src/components/Submission.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Submission extends React.Component {
+  render() {
+    return (
+      <div>
+          This is the submission component
+      </div>
+    )
+  }
+}
+
+
+export default Submission;
