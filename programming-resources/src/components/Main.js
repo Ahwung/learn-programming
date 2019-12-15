cd0fdbdc510db4b882aaa4794e6a59c36248e312
Adam Hwung <Ahwung@gmail.com>
2019-12-15 13:29:29 -0800

    adding connection to API

--- programming-resources/src/components/Main.js
+++ programming-resources/src/components/Main.js
@@ -1,6 +1,24 @@
+// =============================
+// DEPENDENCIES
+// =============================
+
+// Packages
 import React from 'react';
+
+// Components
 import Submission from './Submission.js'
 import Form from './Form.js'
+
+// =============================
+// LINK TO CONNECT TO API
+// =============================
+
+let baseUrl = '';
+if (process.env.NODE_ENV === 'development') {
+  baseUrl = 'http://localhost:8888'
+} else {
+  console.log('this is for heroku');
+}
 
 class Main extends React.Component {
   render() {
