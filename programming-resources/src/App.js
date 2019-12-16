a3ff6285abf2a8cc09a9a7cd1f20b2a02dc87f89
Adam Hwung <Ahwung@gmail.com>
2019-12-16 10:14:36 -0800

    testing bootstrap css

--- programming-resources/src/App.js
+++ programming-resources/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header.js'
 import Aside from './components/Aside.js'
 import Main from './components/Main.js'
