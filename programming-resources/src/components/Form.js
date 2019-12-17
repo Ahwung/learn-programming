0d8121a9859da3ca05edc4a59e14488be0e25fea
Adam Hwung <Ahwung@gmail.com>
2019-12-16 20:50:58 -0800

    added bootstrap css to form1

--- programming-resources/src/components/Form.js
+++ programming-resources/src/components/Form.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Field from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
 
 class Form extends React.Component {
   constructor(){
@@ -39,24 +41,31 @@ class Form extends React.Component {
   }
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="title">Title: </label>
-        <input type="text" placeholder="add title" id="title" value={this.state.title} onChange={this.handleChange}></input>
+      <Field onSubmit={this.handleSubmit}>
+        <Field.Group>
+          <Field.Label htmlFor="title">Title: </Field.Label>
+          <Field.Control type="text" placeholder="add title" id="title" value={this.state.title} onChange={this.handleChange}></Field.Control>
+          <Field.Text className="text-muted">Title of Resource</Field.Text>
 
-        <label htmlFor="tool">Tool: </label>
-        <input type="text" id="tool" placeholder="name of tool"value={this.state.tool} onChange={this.handleChange}></input>
+          <Field.Label htmlFor="tool">Tool: </Field.Label>
+          <Field.Control type="text" id="tool" placeholder="name of tool"value={this.state.tool} onChange={this.handleChange}></Field.Control>
+          <Field.Text className="text-muted">Programming Language or technology</Field.Text>
 
-        <label htmlFor="description">Description: </label>
-        <input type="text" id="description" placeholder="add a description" value={this.state.description} onChange={this.handleChange}></input>
+          <Field.Label htmlFor="description">Description: </Field.Label>
+          <Field.Control type="text" id="description" placeholder="add a description" value={this.state.description} onChange={this.handleChange}></Field.Control>
+          <Field.Text className="text-muted">Description of video or course</Field.Text>
 
-        <label htmlFor="url">URL: </label>
-        <input type="text" id="url" placeholder="url" value={this.state.url} onChange={this.handleChange}></input>
+          <Field.Label htmlFor="url">URL: </Field.Label>
+          <Field.Control type="text" id="url" placeholder="url" value={this.state.url} onChange={this.handleChange}></Field.Control>
+          <Field.Text className="text-muted">Full URL to resource</Field.Text>
 
-        <label htmlFor="cost">Cost: </label>
-        <input type="number" id="cost" placeholder="cost" value={this.state.cost} onChange={this.handleChange}></input>
+          <Field.Label htmlFor="cost">Cost: </Field.Label>
+          <Field.Control type="number" id="cost" placeholder="cost" value={this.state.cost} onChange={this.handleChange}></Field.Control>
+          <Field.Text className="text-muted">Cost of course in USD</Field.Text>
 
-        <input type="submit" value="submit"></input>
-      </form>
+          <Button variant="primary" type="submit" value="submit">Add New Resource</Button> 
+        </Field.Group>
+      </Field>
     )
   }
 }
