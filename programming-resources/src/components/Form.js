ad70594d18c1477eab2597582e7d576b4ecfa150
Adam Hwung <Ahwung@gmail.com>
2019-12-16 21:11:08 -0800

    added bootstrap css for second form

--- programming-resources/src/components/Form.js
+++ programming-resources/src/components/Form.js
@@ -46,24 +46,29 @@ class Form extends React.Component {
           <Field.Label htmlFor="title">Title: </Field.Label>
           <Field.Control type="text" placeholder="add title" id="title" value={this.state.title} onChange={this.handleChange}></Field.Control>
           <Field.Text className="text-muted">Title of Resource</Field.Text>
-
+        </Field.Group>
+        <Field.Group>
           <Field.Label htmlFor="tool">Tool: </Field.Label>
           <Field.Control type="text" id="tool" placeholder="name of tool"value={this.state.tool} onChange={this.handleChange}></Field.Control>
           <Field.Text className="text-muted">Programming Language or technology</Field.Text>
-
+        </Field.Group>
+        <Field.Group>
           <Field.Label htmlFor="description">Description: </Field.Label>
           <Field.Control type="text" id="description" placeholder="add a description" value={this.state.description} onChange={this.handleChange}></Field.Control>
           <Field.Text className="text-muted">Description of video or course</Field.Text>
-
+        </Field.Group>
+        <Field.Group>
           <Field.Label htmlFor="url">URL: </Field.Label>
           <Field.Control type="text" id="url" placeholder="url" value={this.state.url} onChange={this.handleChange}></Field.Control>
           <Field.Text className="text-muted">Full URL to resource</Field.Text>
-
+        </Field.Group>
+        <Field.Group>
           <Field.Label htmlFor="cost">Cost: </Field.Label>
           <Field.Control type="number" id="cost" placeholder="cost" value={this.state.cost} onChange={this.handleChange}></Field.Control>
           <Field.Text className="text-muted">Cost of course in USD</Field.Text>
-
-          <Button variant="primary" type="submit" value="submit">Add New Resource</Button> 
+        </Field.Group>
+        <Field.Group>
+          <Button variant="primary" type="submit" value="submit">Submit</Button> 
         </Field.Group>
       </Field>
     )
